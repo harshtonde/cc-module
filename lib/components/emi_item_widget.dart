1a4eb484349507c85cbcb41d3a64f97dd6087403
Harsh Tonde <harshtonde@gmail.com>
2022-12-24 00:02:59 +0530

    fixed null issue on data table

--- lib/components/emi_item_widget.dart
+++ lib/components/emi_item_widget.dart
@@ -31,7 +31,7 @@ class EmiItemWidget extends StatefulWidget {
 }
 
 class _EmiItemWidgetState extends State<EmiItemWidget> {
-  bool? checkboxValue;
+  bool? checkboxValue = false;
 
   @override
   void initState() {
